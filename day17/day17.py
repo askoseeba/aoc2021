e1965177729dfb7e96f87d26c29d52b0480a3e32
Asko Seeba <1336066+askoseeba@users.noreply.github.com>
2021-12-17 12:28:56 +0200

    Day 17 solved.

--- day17/day17.py
+++ day17/day17.py
@@ -1,15 +1,13 @@
 x_min, x_max, y_min, y_max =  20,  30,  -10,  -5  # Test data
 x_min, x_max, y_min, y_max = 185, 221, -122, -74  # Live data
 
-height, y_speed = 0, abs(y_min)
-for ys in range(y_speed):
-    height += ys
+max_height = sum([ys for ys in range(abs(y_min))])
+print('Part 1:', max_height)
 
-print('Part 1:', height)
-
+y_speed = abs(y_min)
 def test_speed(speed_x, speed_y):
     cur_x, cur_y = 0, 0
-    while cur_x <= x_max and y_min <= cur_y <= height:
+    while cur_x <= x_max and y_min <= cur_y <= max_height:
         if speed_x == 0 and cur_x < x_min:
             return False
         if x_min <= cur_x <= x_max and y_min <= cur_y <= y_max:
@@ -21,10 +19,4 @@ def test_speed(speed_x, speed_y):
         speed_y -= 1
     return False
 
-speed_count = 0
-for x in range(x_max + 1):
-    for y in range(y_min, abs(y_min) + 1):
-        if test_speed(x, y):
-            speed_count += 1
-
-print('Part 2:', speed_count)
+print('Part 2:', sum([1 for x in range(x_max + 1) for y in range(y_min, abs(y_min) + 1) if test_speed(x, y)]))
